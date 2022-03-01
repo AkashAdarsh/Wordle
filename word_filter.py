080959b241797d7a5de7d4f8d8d77fa7feaab9fe
AkashAdarsh <42816603+AkashAdarsh@users.noreply.github.com>
2022-03-01 10:53:46 -0500

    added logging (#3)

---
+++ word_filter.py
@@ -0,0 +1,25 @@
+def get_valid_words():
+    with open('words.txt', 'r+') as file_pointer:
+        content = file_pointer.read().split('\n')
+        file_pointer.close()
+        words_list = []
+        for word in content:
+            if len(word) != 5:
+                continue
+            words_list.append(word)
+        return words_list
+
+
+def write_words_list_to_file(words_list):
+    with open('valid_words.txt', 'w') as file_pointer:
+        file_pointer.writelines([str(i) + '\n' for i in words_list])
+        file_pointer.close()
+
+
+def main():
+    words_list = get_valid_words()
+    write_words_list_to_file(words_list)
+
+
+if __name__ == "__main__":
+    main()
--- wordle.py
+++ wordle.py
@@ -1,23 +1,32 @@
 from user_interface import *
 from dictionary import *
+from statistics import *
+from logs import *
 
 
 def main():
     codeword = get_random_word()
-    trials = 6
-    while trials > 0:
+    add_word_to_used_list(codeword)
+    log_to_file('info', 'The selected word is "%s"' % codeword)
+    trials = 1
+    while trials < 7:
         user_word = get_input_from_user(trials)
+        log_to_file('info', 'Trial %s -> User input: "%s"' % (trials, user_word))
 
         if not check_if_entered_word_is_valid(user_word):
             print('Please enter a 5-letter word!')
+            log_to_file('error', 'User entered invalid word')
             continue
 
         if has_user_guessed_the_right_word(user_word, codeword):
             print('You guessed the right word!!!')
+            update_trial_upon_win(trials)
+            log_to_file('info', 'User guessed the right word!!!')
             break
 
-        if check_if_word_in_dictionary(user_word):
+        if not check_if_word_exists('valid_words.txt', user_word):
             print('Warning! Please provide a valid dictionary word!')
+            log_to_file('error', 'User entered invalid word')
             continue
 
         print('The word you entered -> %s' % user_word)
@@ -25,9 +34,17 @@ def main():
 
         position_arr = position_check_for_letters(user_word, codeword)
         validate_letter_position(position_arr, user_word)
-        trials -= 1
+        trials += 1
 
     print('Game over!!! Correct word was %s' % codeword)
+    total_games_played = increment_total_games_played()
+    print("Total games played are - %s" % total_games_played)
+    print('Win percentage: %s' % get_win_percentage())
+    log_to_file('info', 'Win percentage: %s' % get_win_percentage())
+    guess_distribution = get_guess_distribution()
+    for i in range(len(guess_distribution)):
+        print('%s -> %s' % (i + 1, guess_distribution[i]))
+        log_to_file('info', 'Guess Distribution: Trial #%s -> %s' % (i + 1, guess_distribution[i]))
 
 
 if __name__ == "__main__":
