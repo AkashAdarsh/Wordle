103b5a9c884a93dd52a68d463187c67539a55bca
AkashAdarsh <42816603+AkashAdarsh@users.noreply.github.com>
2022-03-07 23:06:42 -0500

    function of csv
    updated csv functionality in logs.py file

--- logs.py
+++ logs.py
@@ -1,4 +1,6 @@
 import logging
+import csv
+from collections import OrderedDict
 
 
 def log_to_file(level, message):
@@ -9,3 +11,28 @@ def log_to_file(level, message):
         logging.error(message)
     else:
         logging.warning(message)
+
+
+def order(trials, user_input):  # function for making file for storing in csv
+    letter_dictionary = OrderedDict()
+    alphabets = [str(i) for i in 'abcdefghijklmnopqrstuvwxyz']  # compare to every letter
+    for letter in alphabets:
+        letter_dictionary[letter] = [0, 0, 0, 0, 0]  # each index frequency
+    while trials > 0:
+        user_input = input('Enter a word: ')  # of each trial
+        for index in range(len(user_input)):
+            temp = letter_dictionary[user_input[index]]
+            index = index + 1
+        trials -= 1
+    for letter in letter_dictionary:
+        separated_letters = ('%s -> %s' % (letter, letter_dictionary[letter]))  # seperate letter and occurence
+
+    with open('letterFrequency.csv', 'w') as csvfile:                           #storing in csv file
+        writer = csv.DictWriter(csvfile, fieldnames=letter_dictionary.keys())      # writing into csv file the letter
+        # and occurance
+        writer.writeheader()
+        writer.writerows(letter_dictionary)             #row output
+
+
+if __name__ == "__main__":
+    order()
